926c2e8c6146f114bae20f96ce12d788faa5502a
trantung <trantunghn196@gmail.com>
2016-02-04 14:05:50 +0700

    prepare_upload

--- app/controllers/api/ApiUploadController.php
+++ app/controllers/api/ApiUploadController.php
@@ -10,7 +10,30 @@ class ApiUploadController extends ApiController {
 	public function store()
 	{
 		$input = Input::all();
-		dd($input);
-	}
+		//check session
+		$sessionId = Common::checkSessionLogin($input);
+		//upload
+		if (isset($input['image_url'])) {
+			//check format image and size image: client
+			foreach ($input['image_url'] as $key => $value) {
+				$filename = $value->getClientOriginalName();
+				print_r($filename.'/');
+			}
 
+		// $file = Input::file($fileUpload);
+		// $filename = $file->getClientOriginalName();
+		// $extension = $file->getClientOriginalExtension();
+		// if(isset($isUnique)) {
+		// 	$filename = changeFileNameImage($filename);
+		// }
+		// if($isFile != '') {
+		// 	$pathUpload = self::getPathFile($extension);
+		// } else {
+		// 	$pathUpload = public_path().UPLOAD_GAME_AVATAR;
+		// }
+		// $uploadSuccess = $file->move($pathUpload, $filename);
+
+		dd(111);
+		}
+	}
 }

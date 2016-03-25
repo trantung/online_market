860ebfe69695a8c59191dfc09e5db48d7549d43a
trantung <trantunghn196@gmail.com>
2016-03-25 09:57:27 +0700

    fix

--- app/controllers/api/ApiProductController.php
+++ app/controllers/api/ApiProductController.php
@@ -20,7 +20,7 @@ class ApiProductController extends ApiController {
 				}
 				$data = array(
 					'id' => $product->id,
-					'phone' => User::find($product->id)->phone,
+					'phone' => User::find($product->user_id)->phone,
 					'name' => $product->name,
 					'description' => $product->description,
 					'avatar' => url(PRODUCT_UPLOAD . '/' . $product->user_id . '/' . $product->avatar),

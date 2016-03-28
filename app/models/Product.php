5b07d093588783afd8c6b8da1bc151f2e30d0970
dxnm <2711sq@gmail.com>
2016-03-28 09:52:16 +0700

    add address into model

--- app/models/Product.php
+++ app/models/Product.php
@@ -8,7 +8,7 @@ class Product extends Eloquent
 {
 	use SoftDeletingTrait;
     protected $table = 'products';
-    protected $fillable = ['user_id', 'category_id', 'type_id', 'price_id', 'price', 'name', 'description', 'avatar', 'city_id', 'lat', 'long', 'position', 'status', 'start_time'];
+    protected $fillable = ['user_id', 'category_id', 'type_id', 'price_id', 'price', 'name', 'description', 'avatar', 'city_id', 'lat', 'long', 'position', 'status', 'start_time', 'address'];
     protected $dates = ['deleted_at'];
 
 }

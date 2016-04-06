711efbac17435c83751681e6d409cdd1c9f40555
dxnm <2711sq@gmail.com>
2016-04-06 12:01:40 +0700

    fix

--- app/database/migrations/2016_04_06_120107_insert_city_search_tbl.php
+++ app/database/migrations/2016_04_06_120107_insert_city_search_tbl.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class InsertCityCityIdSearchTbl extends Migration {
+class InsertCitySearchTbl extends Migration {
 
 	/**
 	 * Run the migrations.

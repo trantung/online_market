0067310f584d3c71038bf88cd47a29a85cb627a2
dxnm <2711sq@gmail.com>
2016-04-07 16:14:48 +0700

    fix

--- app/services/CommonProduct.php
+++ app/services/CommonProduct.php
@@ -54,6 +54,7 @@ class CommonProduct {
 		foreach ($result as $key => $value) {
 			$value->avatar = url(PRODUCT_UPLOAD . '/' . $value->user_id . '/' . Product::find($value->id)->avatar);
 			$value->block = Common::checkBlackList(Input::get('user_id'), $value->user_id);
+			$value->favorite = CommonFavorite::checkFavoriteLike('User', $value->user_id, TYPE_FAVORITE_LIKE, Input::get('user_id'));
 		}
 		return $result;
 	}
@@ -92,7 +93,7 @@ class CommonProduct {
 		// else{
 		// 	$data = CommonProduct::getProduct($options);
 		// }
-		
+
 		return Common::returnData(200, SUCCESS, $input['user_id'], $input['session_id'], $data);
 	}
 
